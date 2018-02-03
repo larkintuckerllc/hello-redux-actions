7a77c1f5d63f6cb61439193fb83af0bae56790ec
John  Tucker <john@larkintuckerllc.com>
2018-02-03 07:54:13 -0500

    Fix string

--- setup/src/ducks/counter.js
+++ setup/src/ducks/counter.js
@@ -1,16 +1,19 @@
+// ACTIONS
+const COUNTER_INCREMENT = 'COUNTER_INCREMENT';
+const COUNTER_DECREMENT = 'COUNTER_DECREMENT';
 // ACTION CREATORS
 export const incrementCounter = () => ({
-  type: 'COUNTER_INCREMENT',
+  type: COUNTER_INCREMENT,
 });
 export const decrementCounter = () => ({
-  type: 'COUNTER_DECREMENT',
+  type: COUNTER_DECREMENT,
 });
 // REDUCERS
 export default (state = 0, action) => {
   switch (action.type) {
-    case 'COUNTER_INCREMENT':
+    case COUNTER_INCREMENT:
       return state + 1;
-    case 'COUNTER_DECREMENT':
+    case COUNTER_DECREMENT:
       return state - 1;
     default:
       return state;
